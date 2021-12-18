d8def852db6fc13016753d4ddb4badf6008c9b45
Rudolf Nosek <rudolfnosek.rn@gmail.com>
2021-12-18 14:22:38 +0100

    Node: constructor with height

--- Node.cpp
+++ Node.cpp
@@ -1,3 +1,12 @@
 #include "Node.h"
 
-Node::Node(int val,Node *right,Node *left): value(val),right(right),left(left){}
+Node::Node(int val,Node *right,Node *left):
+value(val),
+right(right),
+left(left){}
+
+Node::Node(int val,int height,Node *right,Node *left):
+value(val),
+height(height),
+right(right),
+left(left){}
--- tests_Node.cpp
+++ tests_Node.cpp
@@ -8,6 +8,15 @@ using namespace ::testing;
 TEST(Node_test, ConstructorWithoutParams){
     Node *n = new Node(6);
     ASSERT_EQ(6,n->get_val());
+    ASSERT_EQ(0,n->get_height());
+    ASSERT_EQ(nullptr,n->get_left());
+    ASSERT_EQ(nullptr,n->get_right());
+}
+
+TEST(Node_test, ConstructorWithHeightButNoChildren){
+    Node *n = new Node(5,10);
+    ASSERT_EQ(5,n->get_val());
+    ASSERT_EQ(10,n->get_height());
     ASSERT_EQ(nullptr,n->get_left());
     ASSERT_EQ(nullptr,n->get_right());
 }
