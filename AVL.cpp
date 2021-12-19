fe2b91206d7adf654aefed374f9783115fac4909
Rudolf Nosek <rudolfnosek.rn@gmail.com>
2021-12-19 19:27:40 +0100

    AVL: rotation left

--- AVL.cpp
+++ AVL.cpp
@@ -25,4 +25,24 @@ bool AVL::del(int val){
     return false;
 }
 
+int AVL::max(int a, int b){
+    return (a > b)? a : b;
+}
 
+Node* AVL::rotate_left(Node *x){
+    Node *y = x->get_right();
+    Node *T2 = y->get_left();
+
+    // Perform rotation
+    y->set_left(x);
+    x->set_right(T2);
+
+    // Update heights
+    x->set_height(max(x->get_left()->get_height(),
+                      x->get_right()->get_height()) + 1);
+    y->set_height(max(y->get_left()->get_height(),
+                      y->get_right()->get_height()) + 1);
+
+    // Return new root
+    return y;
+}
